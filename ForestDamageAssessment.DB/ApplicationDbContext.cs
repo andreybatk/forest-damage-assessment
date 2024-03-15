66639d2bfbf4106e2a30b3b4057bc230d47edbfc
Andrey Batukhtin <batyhtik3@gmail.com>
2024-03-15 16:15:21 +0300

    Add project files.

---
+++ ForestDamageAssessment.DB/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestDamageAssessment.DB
+{
+    // dotnet ef migrations add InitIdentity --project PhoneBook.DB --startup-project PhoneBook
+    // dotnet ef database update --project PhoneBook.DB --startup-project PhoneBook
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        //public DbSet<Person> Persons { get; set; } = null!;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    //modelBuilder.Entity<Person>();
+        //}
+    }
+}
---
+++ ForestDamageAssessment.DB/Data/ForestDamageAssessmentData.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ForestDamageAssessment.DB.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestDamageAssessment.DB.Data
+{
+    public class ForestDamageAssessmentData : IForestDamageAssessmentData
+	{
+        private readonly ApplicationDbContext context;
+
+        public ForestDamageAssessmentData(ApplicationDbContext Context)
+        {
+            this.context = Context;
+        }
+    }
+}
